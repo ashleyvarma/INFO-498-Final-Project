ff93fd429dc0abc2be1b4442edf54135aa81b7d5
ashleyvarma <ashleyvarm@gmail.com>
2018-05-30 07:36:41 -0700

    Add specific comment sections

--- server.R
+++ server.R
@@ -1,12 +1,14 @@
+# This is the server logic of our Shiny web application. 
+
 library(dplyr)
 library(shiny)
 library(plotly)
+# install.packages("rsconnect")
 library(rsconnect)
 source('BuildGraph.R')
 
-
-
 shinyServer(function(input,output) {
+  ### Sports Data Server Logic ###
   dataset <- read.csv("data/active.csv")
   output$bar <- renderPlotly({
     bar()
